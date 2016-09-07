0c99cadf65f44b33dcf64114ed0ca9310c688ef0
timlewis <timlewis@umich.edu>
2016-09-07 13:25:31 -0400

    Documentation
    Added comments

--- 31.py
+++ 31.py
@@ -1,13 +1,37 @@
+# Problem:
+# In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+#
+# 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+# It is possible to make £2 in the following way:
+#
+# 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+# How many different ways can £2 be made using any number of coins?
+
+# Solution:
+# Dynamic programming
+
+# Array that contains the number of ways to make a certain amount of money using a certain number of the types of coins
+# Example: ways[x][y] = the number of ways to make x cents using only the first y coins
+# Note: We could save memory by only keeping track of the number of ways for the previous coin
 ways = [[0 for x in range(9)] for x in range(201)]
 
+# The number of cents each coin is worth
 c = [0,1,2,5,10,20,50,100,200]
 
+# For each of the 8 coins
+# Note: for any value there are 0 ways to make that value using 0 coins so this value is skipped
 for i in range(1,9):
+    # For all the different possible values we could want to make
     for j in range(201):
-        ways[j][i] += ways[j][i-1]
+        # Add the number of ways to make that value without using coin i
+        ways[j][i] = ways[j][i-1]
+        # If the value we are trying to make is exactly the same as the coin we can use a single coin to make the value
         if j == c[i]:
             ways[j][i] += 1
+        # If the value we are trying to make is larger than the coins value then
+        # the total number of ways of making the value using the coin at least once is ways[j-c[i]][i]
         elif j > c[i]:
             ways[j][i] += ways[j-c[i]][i]
 
+# Print the number of ways to make 200 cents using all 8 coins
 print(str(ways[200][8]))
--- 42.py
+++ 42.py
@@ -1,23 +1,36 @@
+# Problem:
+# The nth term of the sequence of triangle numbers is given by, tn = ½n(n+1); so the first ten triangle numbers are:
+#
+# 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+#
+# By converting each letter in a word to a number corresponding to its alphabetical position and adding these values we form a word value. For example, the word value for SKY is 19 + 11 + 25 = 55 = t10. If the word value is a triangle number then we shall call the word a triangle word.
+#
+# Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words, how many are triangle words?
+
 import io
 import math
 text = ""
 
+# Finds the sum of all of the character values (a = 1, b = 2, ...) for a given word
 def get_word_value(word):
     sum = 0
-    for c in word:
-        if ord(c) >= 65 and ord(c) <= 90:
-            sum += ord(c) - 64
+    for c in word: # for each character in the word
+        if ord(c) >= 65 and ord(c) <= 90: # if the character is a lower case letter
+            sum += ord(c) - 64 # add the value of the character (a = 1, b = 2, ...) to the sum
     return sum
 
+# Check if a given integer is of the form ½n(n+1)
 def is_triangle(integer):
-    value = int(math.sqrt(2*integer + 0.25))
-    return value*value/2 + value/2 == integer
+    value = int(math.sqrt(2*integer + 0.25)) # find the nearest value for n (rounded down)
+    return value*value/2 + value/2 == integer # if ½n(n+1) = the input then the input is a triangle number
 
+# Open the file
 with io.open('p042_words.txt', 'r') as file:
     text = file.read()
 
 total = 0
 words = text.split(',')
+# Count the number of triangle words in the file
 for word in words:
     if is_triangle(get_word_value(word)):
         total += 1
